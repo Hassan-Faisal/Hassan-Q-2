83258a69d7d3ab1ceb48050af8b60bafa4d7c53c
Hassan-Faisal <iamhassanfaisal@gmail.com>
2024-03-24 10:36:24 +0500

    class03 updated

--- class 1.ts
+++ class 1.ts
@@ -1,35 +1,36 @@
-class Person {
-    name: string;
-    age: number;
+// class Person {
+//     name: string;
+//     age: number;
   
-    constructor(name: string, age: number) {
-      this.name = name;
-      this.age = age;
-    }
+//     constructor(name: string, age: number) {
+//       this.name = name;
+//       this.age = age;
+//     }
   
-    speak() {
-      console.log(`My name is ${this.name} and i'm ${this.age} years old`);
-    }
-  }
+//     speak() 
+//     {
+//       console.log(`My name is ${this.name} and i'm ${this.age} years old`);
+//     }
+//   }
   
-class Q2 extends Person {
-    profession: string;
+// class Q2 extends Person {
+//     profession: string;
   
-    constructor(name: string, age: number, profession: string) {
-      super(name, age);
-      this.profession = profession;
-      console.log(profession);
-    }
+//     constructor(name: string, age: number, profession: string) {
+//       super(name, age);
+//       this.profession = profession;
+//       console.log(profession);
+//     }
   
-    speak() {
-      console.log(`My name is ${this.name} and I am a ${this.profession}`);
-    }
-  }
+//     speak() {
+//       console.log(`My name is ${this.name} and I am a ${this.profession}`);
+//     }
+//   }
   
-const generalPerson = new Person("Hassan", 20);
-const developer = new Q2("Hassan", 20, "A passionate Mern Stack Developer");
+// const generalPerson = new Person("Hassan", 20);
+// const developer = new Q2("Hassan", 20, "A passionate Mern Stack Developer");
   
-  generalPerson.speak();
-  developer.speak();
+//   generalPerson.speak();
+//   developer.speak();
   
-  generalPerson.speak();
+//   generalPerson.speak();
